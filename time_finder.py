79fc26981a86d6ef0f1f463e80a2a09fac0a8e30
safi21 <sappiahmed@gmail.com>
2022-06-21 17:24:09 +0600

    updated  version

--- time_finder.py
+++ time_finder.py
@@ -1,22 +1,26 @@
-#This program takes a city as an input and shows local time of that city.
 from datetime import datetime
 import pytz
 
-#converts city to timezone eg: dhaka to 'Asia/Dhaka'
-def tz_converter(city): 
+# function to find timezone of a given city. For example: it will take input 'dhaka' and return 'Asia/Dhaka'
+def fixed_timezone(city):
     for tz in pytz.all_timezones:
-        if city.capitalize() in tz:
-            break
-    return tz
+        if city.capitalize() == tz[tz.find('/')+1:]:
+            return tz
+    else:
+        print("Try again")
+        exit()
 
-#function to find individual timezone eg: 'Asia/Dhaka'
-def ind_tz(time_zone):
-    tz_city = pytz.timezone(tz_converter(city))
-    time = datetime.now(tz_city)
+
+# function to return time of an individual timezone.
+def ret_time(time_zone):
+    tz = pytz.timezone(time_zone)
+    time = datetime.now(tz)
     return time
 
 if __name__ == "__main__":
-    city = input("Enter a timezone: ")
-    time = ind_tz(city)
+    city = input("Enter a city or Timezone: ")
+    time_zone = fixed_timezone(city)
+    time = ret_time(time_zone)
+    fmt = '%Y-%m-%d %H:%M:%S'
+    time = time.strftime(fmt)
     print(time)
-
